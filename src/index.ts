d3eb4595503ca6b349e3aaa76b9653f3fa92b36a
Nazim <nazim86@gmail.com>
2023-02-15 12:00:03 +0400

    working with middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,71 @@
-import express, {Request, Response } from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 const app = express()
-const port = 3000
+const port = 5000
+
+type blogsType={
+    id: number
+    name: string
+    description: string
+    websiteUrl: string
+}
+
+// const blogs: blogsType = []
+
+// let blablaMiddleware = (req: Request, res: Response, next: NextFunction)=>{
+//     // @ts-ignore
+//     req.blabla = "Nazimito";
+//     next();
+// }
+//
+// let authGuard = (req: Request, res: Response, next: NextFunction)=>{
+//     if (req.query.token === "123"){
+//         next();
+//     }
+//     else {
+//         res.send(401)
+//     }
+// }
+//
+// let counter = 0;
+//
+// let countRequest = (req: Request, res: Response, next: NextFunction)=>{
+//     counter++;
+//     next()
+// }
+//
+// app.use(authGuard)
+// app.use(blablaMiddleware)
+// app.use(countRequest)
+//
+//
+// app.get('/products', (req, res) => {
+//
+//     // @ts-ignore
+//     const  blabla = req.blabla;
+//     res.send({value: blabla +" "+ counter})
+// })
+//
+// app.get('/users', (req, res) => {
+//
+//     // @ts-ignore
+//     const  blabla = req.blabla;
+//     res.send({value: blabla +" "+ counter})
+// })
 
 app.get('/', (req, res) => {
+    const id = new Date()
+    const name = req.body.name
+    const description = req.body.description
+    const websiteUrl = req.body.websiteUrl
+
     res.send('Hello World!')
 })
+
+app.post('/blogs', (req, res) => {
+
+    res.send('Hello World!')
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
