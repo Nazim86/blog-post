0ca0ed7d072c5ba8d441f9c26dbbc29ab0c2a8e4
Nazim <nazim86@gmail.com>
2023-02-17 00:40:19 +0400

    set up authorization

--- src/middlewares/base-auth-middlewares.ts
+++ src/middlewares/base-auth-middlewares.ts
@@ -2,10 +2,14 @@ import {NextFunction, Request, Response} from "express";
 
 export const baseAuthorizationMiddleware = (req:Request, res:Response, next: NextFunction)=>{
 
-    const login = req.headers.login
-    const password = req.headers.password
+    const userLoginPassword = req.headers.authorization
 
-    if (login != "admin" || password != "qwerty") {
+    const encodedLoginPassword: string = Buffer.from("admin:qwerty", 'utf8').toString('base64');
+
+    // const decoded: string = Buffer.from(encoded, 'base64').toString('utf8');
+
+
+    if (userLoginPassword != "Basic "+encodedLoginPassword ) {
         return res.send(401)
     }else{
         next()
