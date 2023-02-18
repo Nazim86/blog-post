bb5e296ccff50a2b58c6c78eb2d79f1c0add1a9f
Nazim <nazim86@gmail.com>
2023-02-18 12:32:01 +0400

    blogId custom validation

--- src/routes/post-routes.ts
+++ src/routes/post-routes.ts
@@ -3,6 +3,7 @@ import {body, param} from "express-validator";
 import {postRepository, posts} from "../repositories/post-repository";
 import {baseAuthorizationMiddleware} from "../middlewares/base-auth-middlewares";
 import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
+import {blogs} from "../repositories/blog-repository";
 
 export const postRoutes = Router({})
 
@@ -10,15 +11,15 @@ const nameValidation = body("title").isString().trim().notEmpty().isLength({max:
 const descriptionValidation = body("shortDescription").isString().trim().notEmpty().isLength({max:100})
 const contentValidation = body("content").isString().trim().notEmpty().isLength({max:1000})
 const idValidation = param("id").exists()
-export const blogIdValidation = body("blogId").isString().trim().notEmpty().
-custom((value, { req }) => {
-    if (value !== req.body.blogId) {
-        throw new Error('Password confirmation does not match password');
-    }
-
-    // Indicates the success of this synchronous custom validator
-    return true;
-})
+export const blogIdValidation = body("blogId").isString().trim().notEmpty()
+// custom((value, { req }) => {
+//     if (value !== req.body.blogId) {
+//         throw new Error('Password confirmation does not match password');
+//     }
+//
+//     // Indicates the success of this synchronous custom validator
+//     return true;
+// })
 
 // custom((blogs, { req }) => {
 //
@@ -45,14 +46,14 @@ postRoutes.post('/',baseAuthorizationMiddleware,blogIdValidation,nameValidation,
         const content = req.body.content;
         const blogId = req.body.blogId;
 
-        // const findBlogId = blogs.find(p=>p.id === blogId)
+        const findBlogId = blogs.filter(p=>p.id === blogId)
 
+        if(findBlogId){
             const newPost = postRepository.createPost(title, shortDescription, content, blogId)
-
-        if (newPost) {
+            if (newPost) {
                 res.status(201).send(newPost)
             }
-
+        }
 
     })
 
