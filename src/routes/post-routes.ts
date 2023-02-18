cbd4426c87cb4dc543202d73e5fa6d610a9f4b43
Nazim <nazim86@gmail.com>
2023-02-18 13:21:52 +0400

    blogId custom validation

--- src/routes/post-routes.ts
+++ src/routes/post-routes.ts
@@ -7,76 +7,46 @@ import {blogs} from "../repositories/blog-repository";
 
 export const postRoutes = Router({})
 
-const nameValidation = body("title").isString().trim().notEmpty().isLength({max:30})
-const descriptionValidation = body("shortDescription").isString().trim().notEmpty().isLength({max:100})
-const contentValidation = body("content").isString().trim().notEmpty().isLength({max:1000})
+const nameValidation = body("title").isString().trim().notEmpty().isLength({max: 30})
+const descriptionValidation = body("shortDescription").isString().trim().notEmpty().isLength({max: 100})
+const contentValidation = body("content").isString().trim().notEmpty().isLength({max: 1000})
 const idValidation = param("id").exists()
-export const blogIdValidation = body("blogId").isString().trim().notEmpty().
-custom((findBlogId, { req }) => {
-    if (blogs.find(p=>p.id!= req.body.blogId) ) {
-        throw new Error('Password confirmation does not match password');
-    }
-
-    // Indicates the success of this synchronous custom validator
+export const blogIdValidation = body("blogId").isString().trim().notEmpty().custom(value => {
+    const blog = blogs.find(b => b.id === value)
+    if (!blog) throw new Error()
     return true;
 })
 
-// custom((blogs, { req }) => {
-//
-//        if (checkBlogsId(req.body.blogId)){
-//            return true;
-//        }else{
-//            throw new Error("Invalid ID")
-//        }
-//     // Indicates the success of this synchronous custom validator
-//
-// })
+const createPostValidation = [ nameValidation, descriptionValidation, contentValidation, blogIdValidation, inputValidationMiddleware,]
 
-
-
-postRoutes.get('/', (req:Request, res:Response) => {
+postRoutes.get('/', (req: Request, res: Response) => {
     res.status(200).send(posts)
 })
 
-postRoutes.post('/',baseAuthorizationMiddleware,nameValidation,descriptionValidation,contentValidation,blogIdValidation,inputValidationMiddleware,
-    (req, res) => {
+postRoutes.post('/', baseAuthorizationMiddleware, createPostValidation,
+    (req: Request, res: Response) => {
 
         const title = req.body.title
         const shortDescription = req.body.shortDescription;
         const content = req.body.content;
         const blogId = req.body.blogId;
 
-        // const findBlogId = blogs.find(p=>p.id === blogId)
-        // //
-        // // console.log(findBlogId)
-        // // const errMessage =
-        // // {
-        // //     "errorsMessages": [
-        // //     {
-        // //         "message": "Blog Id not foung",
-        // //         "field": "blogId"
-        // //     }
-        // // ]
-        // // }
 
-
-
-
-            const newPost = postRepository.createPost(title, shortDescription, content, blogId)
-            if (newPost) {
-                res.status(201).send(newPost)
-            }
+        const newPost = postRepository.createPost(title, shortDescription, content, blogId)
+        if (newPost) {
+            res.status(201).send(newPost)
+        }
 
 
     })
 
-postRoutes.get('/:id',idValidation, (req:Request, res:Response) => {
+postRoutes.get('/:id',  (req: Request, res: Response) => {
 
     const getPost = postRepository.getPostById(req.params.id)
 
-    if(getPost){
+    if (getPost) {
         res.status(200).send(getPost)
-    }else{
+    } else {
         res.send(404)
     }
 
@@ -84,8 +54,8 @@ postRoutes.get('/:id',idValidation, (req:Request, res:Response) => {
 })
 
 
-postRoutes.put('/:id',baseAuthorizationMiddleware,idValidation,nameValidation,descriptionValidation,contentValidation,blogIdValidation,inputValidationMiddleware,
-    (req, res) => {
+postRoutes.put('/:id', baseAuthorizationMiddleware, idValidation, nameValidation, descriptionValidation, contentValidation, blogIdValidation, inputValidationMiddleware,
+    (req: Request, res: Response) => {
 
 
         const title = req.body.title
@@ -93,22 +63,22 @@ postRoutes.put('/:id',baseAuthorizationMiddleware,idValidation,nameValidation,de
         const content = req.body.content;
         const blogId = req.body.blogId;
 
-        const updatePost = postRepository.updatePost(req.params.id,title, shortDescription, content, blogId)
+        const updatePost = postRepository.updatePost(req.params.id, title, shortDescription, content, blogId)
 
         if (updatePost) {
             res.send(204)
-        }else{
+        } else {
             res.send(404)
         }
     })
 
-postRoutes.delete('/:id',baseAuthorizationMiddleware, (req:Request, res:Response) => {
+postRoutes.delete('/:id', baseAuthorizationMiddleware, (req: Request, res: Response) => {
 
     const deletePost = postRepository.deletePostById(req.params.id)
 
-    if (deletePost){
+    if (deletePost) {
         res.send(204)
-    }else{
+    } else {
         res.send(404)
     }
 
