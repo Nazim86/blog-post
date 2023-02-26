fe67c65c92720cf88c3e4db5aecce2c07680e839
Nazim <nazim86@gmail.com>
2023-02-26 23:16:37 +0400

    add createdAt to post API

--- src/repositories/post-in-db-repository.ts
+++ src/repositories/post-in-db-repository.ts
@@ -29,7 +29,7 @@ export const postRepository = {
             content: content,
             blogId: blogId,
             blogName: newId.toString(),
-            createdAt: new Date().toISOString()
+            createdAt: newPost.createdAt
         }
     },
 
