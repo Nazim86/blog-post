c4d827cbf3bf0d6782b72321cddea2752756ef40
Nazim <nazim86@gmail.com>
2023-02-26 22:07:01 +0400

    deploying vercecl

--- src/db/db.ts
+++ src/db/db.ts
@@ -6,7 +6,6 @@ import * as Process from "process";
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 
-
 const url = Process.env.MONGO_URL
 
 if (!url){
--- src/routes/delete-routes.ts
+++ src/routes/delete-routes.ts
@@ -4,8 +4,8 @@ import {blogsCollection, postsCollection} from "../db/db";
 
 export const deleteRoute = Router({})
 
-deleteRoute.delete('/', (req: Request, res: Response) => {
-    blogsCollection.deleteMany({})
-    postsCollection.deleteMany({})
+deleteRoute.delete('/', async (req: Request, res: Response) => {
+    await blogsCollection.deleteMany({})
+    await postsCollection.deleteMany({})
     return res.sendStatus(204)
 })
