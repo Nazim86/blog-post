4fc9f185a4e62d00dec9afcff4586354ddad0e92
Nazim <nazim86@gmail.com>
2023-03-01 12:39:02 +0400

    repo

--- dist/query-repositories/blog-query-repo.js
+++ dist/query-repositories/blog-query-repo.js
@@ -16,23 +16,28 @@ let users = [
     { id: 'dsdf2-sdfs-23', name: 'dimych', age: 34 },
     { id: 'csdc3-ddfs-11', name: 'ivan', age: 30 },
     { id: 'dsdc1-dwfs-31', name: 'ignat', age: 20 },
+    { id: 'qsdf2-sdfs-23', name: 'kolya', age: 22 },
     { id: '6sac2-1d1s-21', name: 'artem', age: 20 },
     { id: '6sac2-1d1s-21', name: 'artem', age: 22 },
     { id: '6sac2-1d1s-21', name: 'artem', age: 28 },
     { id: '6sac2-1d1s-21', name: 'artem', age: 25 },
-    { id: '6sac2-1d1s-21', name: 'artem', age: 30 },
+    { id: '7sac2-1d1s-21', name: 'artem', age: 30 },
+    { id: '11sac2-1d1s-21', name: 'artem', age: 30 },
+    { id: '10sac2-1d1s-21', name: 'artem', age: 30 },
+    { id: '9ac2-1d1s-21', name: 'artem', age: 30 },
+    { id: '8sac2-1d1s-21', name: 'artem', age: 30 },
 ];
-const getUser = (sortBy, thenBy) => {
-    return [...users].sort((u1, u2) => {
-        if (u1[sortBy.fieldName] < u2[sortBy.fieldName])
-            return -1;
-        if (u1.name > u2.name)
-            return 1;
-        if (u1.age < u2.age)
-            return 1;
-        if (u1.age > u2.age)
-            return -1;
+const getSortedItems = (items, sortBy) => {
+    return [...items].sort((u1, u2) => {
+        for (let sortConfig of sortBy) {
+            if (u1[sortConfig.fieldName] < u2[sortConfig.fieldName]) {
+                return sortConfig.direction === 'asc' ? -1 : 1;
+            }
+            if (u1[sortConfig.fieldName] > u2[sortConfig.fieldName]) {
+                return sortConfig.direction === 'asc' ? 1 : -1;
+            }
+        }
         return 0;
     });
 };
-console.log(getUser());
+console.log(getSortedItems(users, [{ fieldName: 'name', direction: 'asc' }, { fieldName: 'age', direction: 'desc' }, { fieldName: 'id', direction: 'asc' }]));
