e5c8d3d589668b311477f3166a8fb0edbca9a7f6
Nazim <nazim86@gmail.com>
2023-04-12 10:51:54 +0400

    commenting secure

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -93,7 +93,7 @@ authRoutes.post('/login', checkIpLimitMiddleware, authValidations, inputValidati
 
     res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
-        sameSite: 'strict', //secure: true,
+        sameSite: 'strict', secure: true,
         maxAge: 24 * 60 * 60 * 1000
     });
 
@@ -115,7 +115,7 @@ authRoutes.post('/refresh-token', checkRefreshTokenMiddleware,
 
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            sameSite: 'strict', //secure: true,
+            sameSite: 'strict', secure: true,
             maxAge: 24 * 60 * 60 * 1000
         });
 
