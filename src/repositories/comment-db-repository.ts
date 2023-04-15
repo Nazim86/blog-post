6129de8ac8a923c82401a7db5c4edb097812a32d
Nazim <nazim86@gmail.com>
2023-04-15 09:57:57 +0400

    add CommentModel to comment-db-repository.ts

--- src/repositories/comment-db-repository.ts
+++ src/repositories/comment-db-repository.ts
@@ -1,14 +1,14 @@
 import {CommentsDbType} from "./types/comments-db-type";
 import {CommentsViewType} from "./types/comments-view-type";
-import {commentsCollection} from "../db/db";
+import {CommentModel} from "../db/db";
 import {ObjectId} from "mongodb";
 
 export const commentDbRepository = {
 
     async createPostComment(postComment:CommentsDbType,userId:string, userLogin:string):Promise<CommentsViewType>{
-        const comment = await commentsCollection.insertOne(postComment)
+        const comment = await CommentModel.create(postComment)
         return{
-            id: comment.insertedId.toString(),
+            id: comment._id.toString(),
             content: postComment.content,
             commentatorInfo: {
                 userId: userId,
@@ -20,13 +20,13 @@ export const commentDbRepository = {
     },
 
     async updateComment(commentId:string,content:string):Promise<boolean>{
-        const result = await commentsCollection.updateOne({_id:new ObjectId(commentId)},{$set:{content:content}})
+        const result = await CommentModel.updateOne({_id:new ObjectId(commentId)},{$set:{content:content}})
 
         return result.matchedCount === 1
     },
 
     async deleteComment(commentId:string):Promise<boolean>{
-        const result = await commentsCollection.deleteOne({_id:new ObjectId(commentId)})
+        const result = await CommentModel.deleteOne({_id:new ObjectId(commentId)})
 
         return result.deletedCount ===1
     }
