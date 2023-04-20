d785e60a50b25cf2e62e165ffa856768f083882d
Nazim <nazim86@gmail.com>
2023-04-20 18:54:54 +0400

    change blog-in-db-repository.ts to class

--- src/repositories/blog-in-db-repository.ts
+++ src/repositories/blog-in-db-repository.ts
@@ -4,9 +4,7 @@ import {blogMapping} from "../mapping/blog-mapping";
 import {BlogsViewType} from "./types/blogs-view-type";
 import {BlogsDbType} from "./types/blogs-db-type";
 
-
-
-export const blogRepository = {
+class BlogRepository {
 
     async createBlog(newBlog: BlogsDbType): Promise<BlogsViewType> {
 
@@ -20,36 +18,34 @@ export const blogRepository = {
             createdAt: newBlog.createdAt,
             isMembership: newBlog.isMembership
         }
-
-    },
+    }
 
     async getBlog(): Promise<BlogsViewType[]> {
 
         const array = await BlogModel.find({}).lean()
 
         return blogMapping(array)
-    },
+    }
 
     async getBlogById(id: string): Promise<BlogsViewType | null> {
         try {
             const foundBlog = await BlogModel.findOne({_id: new ObjectId(id)})
-            if (foundBlog) {
-                return {
-                    id: foundBlog._id.toString(),
-                    name: foundBlog.name,
-                    description: foundBlog.description,
-                    websiteUrl: foundBlog.websiteUrl,
-                    createdAt: foundBlog.createdAt,
-                    isMembership: foundBlog.isMembership
-                }
-            } else {
+            if (!foundBlog) {
                 return null
+            }
+            return {
+                id: foundBlog._id.toString(),
+                name: foundBlog.name,
+                description: foundBlog.description,
+                websiteUrl: foundBlog.websiteUrl,
+                createdAt: foundBlog.createdAt,
+                isMembership: foundBlog.isMembership
             }
         } catch (e) {
             return null
         }
+    }
 
-    },
     async updateBlog(id: string, name: string, description: string, websiteUrl: string): Promise<boolean> {
 
         try {
@@ -57,21 +53,20 @@ export const blogRepository = {
                 {$set: {name: name, description: description, websiteUrl: websiteUrl}}
             )
             return result.matchedCount === 1
-        }
-        catch (e) {
+        } catch (e) {
             return false
         }
-    },
+    }
 
     async deleteBlogById(id: string): Promise<boolean> {
         try {
             const result = await BlogModel.deleteOne({_id: new ObjectId(id)},)
             return result.deletedCount === 1
 
-        }catch (e){
+        } catch (e) {
             return false
         }
     }
 }
 
-
+export const blogRepository = new BlogRepository()
